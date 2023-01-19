6f95d6424ec578ef98c00f1978e43f0a81ad55c5
jsr-p <jonas.raaschou@sodas.ku.dk>
2023-01-19 14:49:36 +0100

    Add code to estimate marginal loglik for VAE

--- code/src/pmldiku/vae.py
+++ code/src/pmldiku/vae.py
@@ -1,14 +1,16 @@
 """Module to implement different VAE architectures."""
 
 from typing import Protocol, TypeAlias
-import pytorch_lightning as pl
+
 import numpy as np
+import tqdm
+import pytorch_lightning as pl
 import torch
+import torch.distributions as tdist
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
 from torchvision.utils import save_image
-import torch.distributions as tdist
 
 from pmldiku import model_utils
 
@@ -31,6 +33,29 @@ class BaseVAE(pl.LightningModule):
         self.fc3 = nn.Linear(self.hidden_dim, 100)
         self.fc3a = nn.Linear(100, 400)
         self.fc4 = nn.Linear(400, 784)
+
+    def forward(self, x: torch.Tensor) -> VAEOutput:
+        """Forward pass of the VAE.
+
+        Args:
+            x: Image tensor of dim. (B, W, H)
+
+        Returns:
+            (VAEOutput):
+                decoded image, encoder mean, encoder log(variance).
+        """
+        z, mu, logvar = self.sample(x.view(-1, 784))
+        return self.decode(z), mu, logvar
+
+    def sample(self, x: torch.Tensor):
+        """Samples from the encoder distribution.
+
+        Returns:
+            sample, encoder mean, encoder log(variance)
+        """
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
 
     def encode(self, x: torch.Tensor) -> EncoderOutput:
         """Encodes the input image tensor.
@@ -67,12 +92,6 @@ class BaseVAE(pl.LightningModule):
         h3 = F.relu(self.fc3(z))
         h4 = F.relu(self.fc3a(h3))
         return torch.sigmoid(self.fc4(h4))
-
-    def forward(self, x: torch.Tensor) -> VAEOutput:
-        """Forward pass of the VAE."""
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
 
 
 class CVAE(pl.LightningModule):
@@ -206,8 +225,8 @@ class VAELoss:
     def cb_loss(self, recon_x: torch.Tensor, x: torch.Tensor):
         """Implements the loss function for continuous Bernoulli r.v.s.
 
-        The loss function for the VAE with the assumption of 
-        X | Z ~ CB (continuous Bernoulli) is equal to the loss 
+        The loss function for the VAE with the assumption of
+        X | Z ~ CB (continuous Bernoulli) is equal to the loss
         function of standard Bernoulli with an added constant term.
         See p. 4 here:
             https://arxiv.org/pdf/1907.06845.pdf
@@ -217,7 +236,6 @@ class VAELoss:
 
     def bce_loss(self, recon_x: torch.Tensor, x: torch.Tensor, reduction: str = "sum"):
         return F.binary_cross_entropy(recon_x, x, reduction=reduction)
-
 
     def __call__(
         self,
@@ -249,6 +267,12 @@ class VAE(Protocol):
     """Protocol for a VAE"""
 
     def __call__(self, X: torch.Tensor) -> VAEOutput:
+        ...
+
+    def sample(self, x: torch.Tensor) -> VAEOutput:
+        ...
+
+    def decode(self, x: torch.Tensor) -> torch.Tensor:
         ...
 
 
@@ -346,3 +370,81 @@ def encode_means(
     means_plot = torch.concatenate(all_encoded_means).detach().numpy()
     labels = torch.concatenate(labels).numpy()
     return means_plot, labels
+
+
+# --------------------- Marginal likelihood estimation --------------------- #
+
+
+class MarginalLogLikVAE:
+    """
+    Class to handle data and methods related to marginal
+    loglikehood estimation for variational autoencoders.
+    """
+
+    def __init__(
+        self,
+        val_loader: torch.utils.data.DataLoader,
+        model: LitVAE,
+        latent_dim: int = 2,
+        L: int = 5,
+    ):
+        self.val_loader = val_loader
+        self.model = model
+        self.latent_dim = latent_dim
+        self.L = L
+        self._construct_latent_dist()
+        self._init_sample()
+
+    def _construct_latent_dist(self):
+        """Construct latent dist. used for all samples"""
+        zero_vec = torch.zeros(self.latent_dim)
+        self.mvn_latent_prior = tdist.MultivariateNormal(
+            loc=zero_vec, covariance_matrix=torch.eye(self.latent_dim)
+        )
+
+    def _init_sample(self):
+        """Initializes tensor to store log p(x_i) for each sample x_i"""
+        self.N_val = len(self.val_loader.dataset)  # type: ignore
+        self.logpx_all = torch.zeros(self.N_val)
+
+    def estimate(self):
+        counter = 0
+        for X_b, _ in tqdm.tqdm(self.val_loader):
+            logpx_batch = self.estimate_batch(X_b)
+            b_shape = X_b.shape[0]
+            self.logpx_all[counter: counter + b_shape] = logpx_batch
+            counter += b_shape
+        logpx = -np.log(self.N_val) + torch.logsumexp(self.logpx_all, dim=0)
+        return logpx
+
+    def estimate_batch(self, X: torch.Tensor) -> torch.Tensor:
+        B = X.shape[0]
+        logpx_all = torch.zeros(B)
+        for i in range(B):
+            x = X[i].unsqueeze(0)
+            logpx = self.estimate_single(x)
+            logpx_all[i] = logpx
+        return logpx_all
+
+    def estimate_single(self, x: torch.Tensor) -> torch.Tensor:
+        """Estimates log p(x) for a given image tensor x.
+
+        Note:
+            To compute the log of an average of a sum of exponentials
+            we use the logsumexp-trick.
+        """
+        samples = torch.zeros(self.L)
+        for j in range(self.L):
+            z, mean, logvar = self.model.vae.sample(x)
+            encoder_var = torch.exp(logvar / 2).squeeze().diag()
+            encoder_mean = mean.squeeze()
+            mvn_encoder = tdist.MultivariateNormal(
+                loc=encoder_mean, covariance_matrix=encoder_var
+            )
+            logqzgx = mvn_encoder.log_prob(z)
+            logpz = self.mvn_latent_prior.log_prob(z)
+            recon_x = self.model.vae.decode(z)
+            logpxgz = -self.model.loss_fn.logpx_loss_fn(recon_x, x.view(-1, 784))
+            samples[j] = logpxgz + logpz - logqzgx
+        logpx = -np.log(self.L) + torch.logsumexp(samples, dim=0)
+        return logpx
